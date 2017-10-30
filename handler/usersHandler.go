99b9e15cf4724f78bb626aff5f8b2497eee1cc87
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-10-31 01:58:46 +0900

    [add]stub json for users

--- handler/usersHandler.go
+++ handler/usersHandler.go
@@ -5,19 +5,39 @@ import (
 
 	"net/http"
 
-	"bytes"
-
 	"github.com/gin-gonic/gin"
 )
 
 func UsersHandler(c *gin.Context) {
 	log.Printf("%#v", c)
 
-	body := c.Request.Body
+	stubJson := `
+{
+    "data": {
+        "users": [
+            {
+                "id": 1,
+                "name": "名前1",
+                "screenName": "スクリーンネーム1",
+                "statusText": "ステータス1",
+            },
+            {
+                "id": 2,
+                "name": "名前2",
+                "screenName": "スクリーンネーム2",
+                "statusText": "ステータス2",
+            },
+            {
+                "id": 3,
+                "name": "名前3",
+                "screenName": "スクリーンネーム3",
+                "statusText": "ステータス3",
+            },
+        ]
+    }
+}
+`
 
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(body)
-	s := buf.String() + "\n"
 
-	c.String(http.StatusOK, s)
+	c.String(http.StatusOK, stubJson)
 }
